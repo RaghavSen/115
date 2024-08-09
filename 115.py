5aa6f2946984e6ec53aa7fb2030a882ecaccdeb2
Raghav Sen <122851729+RaghavSen@users.noreply.github.com>
2024-08-09 17:52:29 +0530

    Update 115.py

--- 115.py
+++ 115.py
@@ -1,12 +1,7 @@
 import os
 
-# Define the source file path
 source_file = 'main.txt'
-
-# Define the destination file path
 destination_file = 'newfile.txt'
-
-# Rename (or move) the file
 os.rename(source_file, destination_file)
 
 print(f"File moved from {source_file} to {destination_file}")
